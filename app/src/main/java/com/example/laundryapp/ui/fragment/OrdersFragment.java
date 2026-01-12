5177a02dc0722096a37753387f0341a4e4f3a397
1002230023Badrul <badrulmyasin83@gmail.com>
2026-01-12 11:52:40 +0700

    LaundryAPP JAVA

---
+++ app/src/main/java/com/example/laundryapp/ui/fragment/OrdersFragment.java
@@ -0,0 +1,4 @@
+package com.example.laundryapp.ui.fragment;
+
+public class OrdersFragment {
+}
---
+++ app/src/main/java/com/example/laundryapp/ui/order/OrderDetailActivity.java
@@ -0,0 +1,40 @@
+package com.example.laundryapp.ui.order;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ProgressBar;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.laundryapp.R;
+import com.example.laundryapp.data.model.HistoryOrder;
+
+public class OrderDetailActivity extends AppCompatActivity {
+
+    HistoryOrder order;
+
+    TextView tvStatus;
+    ProgressBar progressOrder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_detail);
+
+        HistoryOrder order =
+                (HistoryOrder) getIntent().getSerializableExtra("order");
+
+        tvStatus = findViewById(R.id.tvStatus);
+        progressOrder = findViewById(R.id.progressOrder);
+
+        if ("PROCESSING".equals(order.status)) {
+            tvStatus.setText("Processing");
+            progressOrder.setProgress(50);
+        } else {
+            tvStatus.setText("Ready for Pickup");
+            progressOrder.setProgress(100);
+        }
+    }
+}
